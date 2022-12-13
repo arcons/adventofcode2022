72e87bb082712766edd7d8f2f2a752037644dadc
Alex Consunji <arcon1993@gmail.com>
2022-12-13 08:40:01 -0500

    Day 1

--- src/adventofcode/year_2022/day_01_2022.py
+++ src/adventofcode/year_2022/day_01_2022.py
@@ -7,7 +7,20 @@ from adventofcode.util.input_helpers import get_input_for_day
 
 @register_solution(2022, 1, 1)
 def part_one(input_data: list[str]):
-    answer = ...
+    answer = 0
+    sums = []
+    previousSum = 0
+    for i in input_data:
+        # print(i)
+        if(i == ''):
+            sums.append(previousSum)
+            previousSum = 0
+        else:
+            previousSum+=int(i)
+
+    # print(sums)
+    sums.sort()
+    answer = sums[len(sums)-1]
 
     if not answer:
         raise SolutionNotFoundException(2022, 1, 1)
@@ -17,15 +30,28 @@ def part_one(input_data: list[str]):
 
 @register_solution(2022, 1, 2)
 def part_two(input_data: list[str]):
-    answer = ...
+    answer = 0
+    sums = []
+    previousSum = 0
+    for i in input_data:
+        # print(i)
+        if(i == ''):
+            sums.append(previousSum)
+            previousSum = 0
+        else:
+            previousSum+=int(i)
+
+    # print(sums)
+    sums.sort()
+    answer = sums[len(sums)-1]+sums[len(sums)-3]+sums[len(sums)-2]
 
     if not answer:
-        raise SolutionNotFoundException(2022, 1, 2)
+        raise SolutionNotFoundException(2022, 1, 1)
 
     return answer
 
 
 if __name__ == '__main__':
     data = get_input_for_day(2022, 1)
-    part_one(data)
+    print(part_one(data))
     part_two(data)
