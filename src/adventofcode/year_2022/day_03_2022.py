346c17688173f852dce3151f68c2c44862cdb66a
Alex Consunji <arcon1993@gmail.com>
2022-12-26 13:39:07 -0600

    Fixed pathing and such

--- src/adventofcode/year_2022/day_03_2022.py
+++ src/adventofcode/year_2022/day_03_2022.py
@@ -1,5 +1,3 @@
-
-
 from adventofcode.util.exceptions import SolutionNotFoundException
 from adventofcode.registry.decorators import register_solution
 from adventofcode.util.input_helpers import get_input_for_day
@@ -14,6 +12,10 @@ def part_one(input_data: list[str]):
     # Sort values 
     # Loop through and see if the item exists in both
     # Sum total of values
+
+    print(input_data)
+    answer = input_data
+
 
 
     if not answer:
@@ -35,4 +37,4 @@ def part_two(input_data: list[str]):
 if __name__ == '__main__':
     data = get_input_for_day(2022, 3)
     part_one(data)
-    part_two(data)
+    # part_two(data)
