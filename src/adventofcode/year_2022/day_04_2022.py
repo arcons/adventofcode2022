61adee29bd76e4818c735d3424d335c777811e52
Alex Consunji <arcon1993@gmail.com>
2022-12-29 22:26:53 -0500

    Day 4

--- src/adventofcode/year_2022/day_04_2022.py
+++ src/adventofcode/year_2022/day_04_2022.py
@@ -5,9 +5,48 @@ from adventofcode.registry.decorators import register_solution
 from adventofcode.util.input_helpers import get_input_for_day
 
 
+def num_range(inp: str):
+    split_range = inp.split('-')
+    num_set = set()
+    val1 = int(split_range[0])
+    val2 = int(split_range[1]) + 1
+    for n in range(val1, val2):
+        # print(n)
+        num_set.add(n)
+    return num_set
+
+
+def maximum(a: int, b: int):
+    if a > b:
+        return a 
+    return b
+
+def min(a: int, b: int):
+    if a < b:
+        return a 
+    return b
+
 @register_solution(2022, 4, 1)
 def part_one(input_data: list[str]):
-    answer = ...
+
+    count = 0
+    for i in input_data:
+        print(i)
+        split = i.split(',')
+        # print('split', split)
+        first = num_range(split[0])
+        second = num_range(split[1])
+        minval = min(len(first),len(second))
+        # print('first', first)
+        # print('second', second)
+        intersect_one = first.intersection(second)
+        intersect_two = second.intersection(first)
+        print('int1', intersect_one)
+        if len(intersect_one) == minval or len(intersect_two) == minval:
+            print('first + second', first, second)
+            count+=1
+
+    answer = count
 
     if not answer:
         raise SolutionNotFoundException(2022, 4, 1)
@@ -17,7 +56,24 @@ def part_one(input_data: list[str]):
 
 @register_solution(2022, 4, 2)
 def part_two(input_data: list[str]):
-    answer = ...
+    count = 0
+    for i in input_data:
+        print(i)
+        split = i.split(',')
+        # print('split', split)
+        first = num_range(split[0])
+        second = num_range(split[1])
+        minval = min(len(first),len(second))
+        # print('first', first)
+        # print('second', second)
+        intersect_one = first.intersection(second)
+        intersect_two = second.intersection(first)
+        print('int1', intersect_one)
+        if len(intersect_one) > 0 or len(intersect_two) > 0:
+            print('first + second', first, second)
+            count+=1
+
+    answer = count
 
     if not answer:
         raise SolutionNotFoundException(2022, 4, 2)
